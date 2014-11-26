e2efdc25ebe9c7f87c751606abc8186318fd16fb
Al Johri <al.johri@gmail.com>
2014-11-26 01:50:41 -0600

    add latest dump with 44 new users (+friends) from andy's account

--- get_friends.py
+++ get_friends.py
@@ -9,28 +9,32 @@ scraper = get_scraper()
 
 def get_friends(username):
 
+	print username
+
 	session = Session()
 	session.expire_on_commit = False
 
 	fan = session.query(FacebookUser).filter_by(username=username).first()
 
-	print "Friends of %s" % fan.username
+	fan_username = fan.username or fan.uid
+
+	print "Friends of %s" % fan_username
 
 	fan.data = "in progress"
 	session.commit()
 
-	for result in scraper.get_friends_nograph(fan.username):
+	for result in scraper.get_friends_nograph(fan_username):
 		print result
 		current_user = save_user(result, session, log=False)
 		fan.friend(current_user)
-		print "\t-", fan.username, "is friends with", current_user.username
+		print "\t-", fan_username, "is friends with", current_user.username
 		session.commit()
 
 	fan.data = "done"
 	session.commit()
 	session.close()
 
-	return "downloaded %d friends for %s" % (fan.friends.count(), fan.username)
+	return "downloaded %d friends for %s" % (fan.friends.count(), username)
 
 # try:
 # 	# requests.get('asdfasdf')
